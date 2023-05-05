ed12f0ab4f6380024488193a1f26fb0d2a1645e1
Sidharth Hulyalkar <sidharth.hulyalkar@datajoint.com>
2023-05-05 15:41:24 -0500

    modified FacialSignal make to lulab custom

--- element_facemap/facial_behavior_estimation.py
+++ element_facemap/facial_behavior_estimation.py
@@ -438,73 +438,74 @@ class FacialSignal(dj.Imported):
         """Populates the FacialSignal table by transferring the results from default
         Facemap outputs to the database."""
 
-        dataset, _ = get_loader_result(key, FacemapTask)
-        # Only motion SVD region type is supported.
-        dataset["rois"] = [x for x in dataset["rois"] if x["rtype"] == "motion SVD"]
+    dataset, _ = fbe.get_loader_result(key, fbe.FacemapTask)
 
-        self.insert1(key)
+    # Keep only the SVD regions (e.g. no pupil)
+    dataset["rois"] = [roi for roi in dataset["rois"] if "SVD" in roi["rtype"]]
 
-        self.Region.insert(
-            [
-                dict(
-                    key,
-                    roi_no=i,
-                    xrange=dataset["rois"][i]["xrange"],
-                    yrange=dataset["rois"][i]["yrange"],
-                    xrange_bin=dataset["rois"][i]["xrange_bin"]
-                    if "xrange_bin" in dataset["rois"][i]
-                    else None,
-                    yrange_bin=dataset["rois"][i]["yrange_bin"]
-                    if "yrange_bin" in dataset["rois"][i]
-                    else None,
-                    motion=dataset["motion"][i + 1],
-                )
-                for i in range(len(dataset["rois"]))
-            ]
-        )
+    self.insert1(key)
 
-        # MotionSVD
-        if any(np.any(x) for x in dataset.get("motSVD", [False])):
-            entry = [
-                dict(
-                    key,
-                    roi_no=roi_no,
-                    pc_no=i,
-                    singular_value=dataset["motSv"][i] if "motSv" in dataset else None,
-                    motmask=dataset["motMask_reshape"][roi_no + 1][:, :, i],
-                    projection=dataset["motSVD"][roi_no + 1][i],
-                )
-                for roi_no in range(len(dataset["rois"]))
-                for i in range(dataset["motSVD"][roi_no + 1].shape[1])
-            ]
-            self.MotionSVD.insert(entry)
-
-        # MovieSVD
-        if any(np.any(x) for x in dataset.get("movSVD", [False])):
-            entry = [
-                dict(
-                    key,
-                    roi_no=roi_no,
-                    pc_no=i,
-                    singular_value=dataset["movSv"][i] if "movSv" in dataset else None,
-                    movmask=dataset["movMask_reshape"][roi_no + 1][:, :, i],
-                    projection=dataset["movSVD"][roi_no + 1][i],
-                )
-                for roi_no in range(len(dataset["rois"]))
-                for i in range(dataset["movSVD"][roi_no + 1].shape[1])
-                if "SVD" in dataset["rois"][i]["rtype"]
-            ]
-            self.MovieSVD.insert(entry)
-
-        # Summary
-        self.Summary.insert1(
+    self.Region.insert(
+        [
             dict(
                 key,
-                sbin=dataset["sbin"],
-                avgframe=dataset["avgframe"][0],
-                avgmotion=dataset["avgmotion"][0],
+                roi_no=i,
+                xrange=dataset["rois"][i]["xrange"],
+                yrange=dataset["rois"][i]["yrange"],
+                xrange_bin=dataset["rois"][i]["xrange_bin"]
+                if "xrange_bin" in dataset["rois"][i]
+                else None,
+                yrange_bin=dataset["rois"][i]["yrange_bin"]
+                if "yrange_bin" in dataset["rois"][i]
+                else None,
+                motion=dataset["motion"][i],
             )
+            for i in range(len(dataset["rois"]))
+        ]
+    )
+
+    # MotionSVD
+    if any(np.any(x) for x in dataset.get("motSVD", [False])):
+        entry = [
+            dict(
+                key,
+                roi_no=roi_no,
+                pc_no=i,
+                singular_value=dataset["motSv"][i] if "motSv" in dataset else None,
+                motmask=dataset["motMask_reshape"][roi_no + 1][:, :, i],
+                projection=dataset["motSVD"][roi_no + 1][i],
+            )
+            for roi_no in range(len(dataset["rois"]))
+            for i in range(1, dataset["motSVD"][roi_no + 1].shape[1])
+        ]
+        self.MotionSVD.insert(entry)
+
+    # MovieSVD
+    if any(np.any(x) for x in dataset.get("movSVD", [False])):
+        entry = [
+            dict(
+                key,
+                roi_no=roi_no,
+                pc_no=i,
+                singular_value=dataset["movSv"][i] if "movSv" in dataset else None,
+                movmask=dataset["movMask_reshape"][roi_no + 1][:, :, i],
+                projection=dataset["movSVD"][roi_no + 1][i],
+            )
+            for roi_no in range(len(dataset["rois"]))
+            for i in range(1, dataset["movSVD"][roi_no + 1].shape[1])
+            if "SVD" in dataset["rois"][i]["rtype"]
+        ]
+        self.MovieSVD.insert(entry)
+
+    # Summary
+    self.Summary.insert1(
+        dict(
+            key,
+            sbin=dataset["sbin"],
+            avgframe=dataset["avgframe"][0],
+            avgmotion=dataset["avgmotion"][0],
         )
+    )
 
 # ---------------- HELPER FUNCTIONS ----------------
 
