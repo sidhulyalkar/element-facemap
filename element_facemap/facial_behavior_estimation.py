15a24accec9436c99dbd389183482290496ca3fe
Sidharth Hulyalkar <sidharth.hulyalkar@datajoint.com>
2023-10-25 18:13:33 -0500

    modify file_paths to new data upload format

--- element_facemap/facial_behavior_estimation.py
+++ element_facemap/facial_behavior_estimation.py
@@ -196,12 +196,12 @@ class RecordingInfo(dj.Imported):
                 find_full_path(get_facemap_root_data_dir(), file_path)
             ).as_posix()
 
-            if "face_camera" in file_path:
+            if "Whisker" in file_path:
                 fps = 100
             elif (
-                "front_camera" in file_path
-                or "right_camera" in file_path
-                or "left_camera" in file_path
+                "Front" in file_path
+                or "Left" in file_path
+                or "Right" in file_path
             ):
                 fps = 30
             else:
